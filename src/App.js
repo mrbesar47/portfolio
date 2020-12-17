75ee3bdab324d32b12de419538658fb2a1c67807
Besar <limanibesar@gmail.com>
2020-12-17 16:44:17 +0200

    finally deployed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
-import besar from './besar.jpg';
+import Nav from './components/nav/nav';
 import './App.css';
+import { Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={besar} alt="logo" width={300} height={300} />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Page currenntly being updated
-        </p>
-        <a
-          className="App-link"
-          href="https://www.besarlimani.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Besar Limani
-        </a>
+        <Nav />
       </header>
     </div>
   );
---
+++ src/components/nav/nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './nav.css'
+
+const Nav = () =>{
+    return(
+        <nav>
+            <p>
+                Page currently being updated!
+            </p>
+            <ol>
+                <li><a href="https://github.com/mrbesar47/portfolio" target="_blank" rel="noopener noreferrer">Github</a></li>
+                <li><a href="http://www.besarlimani.com">Author</a></li>
+            </ol>
+        </nav>
+    )
+}
+
+export default Nav
