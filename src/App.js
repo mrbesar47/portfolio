6823831daadba00376916e23aadee8899fbed2d5
Besar <limanibesar@gmail.com>
2020-12-17 12:21:01 +0200

    page being updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
+import besar from './besar.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Page currently being updated!</h1>
+        <img src={besar} alt="logo" width={300} height={300} />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
